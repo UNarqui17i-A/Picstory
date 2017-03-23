fd38e70c63bd335852516a07c928e696af38ef59
Rodrigo Espinel <riespinelv@unal.edu.co>
2017-03-23 02:54:56 -0500

    checkpoint

--- MS - Feed/feed/personal_feed/views.py
+++ MS - Feed/feed/personal_feed/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import urllib2
+import json
 # Create your views here.
 
 
 def personal_feed(request, username=None):
-    return HttpResponse(username)
+    end_point_docker = '192.168.99.101:8010/post?user_id='
+    url = end_point_docker + username;
+    serialized_data = urllib2.urlopen(url).read()
+    data = json.loads(serialized_data)
+    html = "<html><body><pre>Data: %s.</pre></body></html>" % json.dumps(data, indent=2)
+    return HttpResponse(html)
+
+
