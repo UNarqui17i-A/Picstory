ec915d31f45e1ce4c478f43e480fced15c71b934
Rodrigo Espinel <riespinelv@unal.edu.co>
2017-04-12 18:42:32 -0500

    post component created

--- frontend/picstory/src/app/feed/feed.module.ts
+++ frontend/picstory/src/app/feed/feed.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WallComponent } from './wall/wall.component';
 import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [WallComponent, NewsfeedComponent]
+  declarations: [WallComponent, NewsfeedComponent, PostComponent]
 })
 export class FeedModule { }
---
+++ frontend/picstory/src/app/feed/post/post.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  posts: Array<string> =
+    [
+      'https://static.pexels.com/photos/147504/pexels-photo-147504.jpeg',
+      'https://static.pexels.com/photos/129897/pexels-photo-129897.jpeg',
+      'https://static.pexels.com/photos/130000/pexels-photo-130000.jpeg',
+      'https://static.pexels.com/photos/147684/pexels-photo-147684.jpeg',
+      'https://static.pexels.com/photos/146868/pexels-photo-146868.jpeg',
+    ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
