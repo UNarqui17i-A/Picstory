d8a270c99578d38197227048604f5e1b0ed86cb3
Rodrigo Espinel <riespinelv@unal.edu.co>
2017-04-17 10:21:20 -0500

    score photos added

--- frontend/picstory/src/app/feed/post/post.component.ts
+++ frontend/picstory/src/app/feed/post/post.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AWSService } from "../services/aws.service";
+import { ComService } from "../services/com.service";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
-  providers: [ AWSService ]
+  providers: [ AWSService, ComService ]
 })
 export class PostComponent implements OnInit {
   @Input() post: any;
   @Input() isWall: boolean = false;
 
   public max:number = 5;
-  public rate:number = 0;
+  public rate:number = null;
 
   public overStar:number;
   public percent:number;
+  oldValue: number;
 
   public hoveringOver(value:number):void {
     this.overStar = value;
@@ -26,17 +28,58 @@ export class PostComponent implements OnInit {
     this.overStar = void 0;
   }
 
-  constructor(private awsService: AWSService) { }
+  constructor(private awsService: AWSService, private comService: ComService) {
+
+  }
 
   ngOnInit() {
     this.awsService.getImage(this.post.image_url)
       .subscribe(
         response => {this.post.image_url = response._body}
-      )
+      );
+
+    var scorer = this.getScoreObject();
+    if( scorer != null && typeof scorer != 'undefined' && JSON.stringify(scorer) != '[]'){
+      this.rate = scorer[0].scored;
+      this.oldValue = this.rate;
+    }
+
+  }
+
+  getScoreObject(){
+    var scorer = this.post.scores.filter(function (data) {
+      if (data.user_id == 'default')
+        return data;
+    });
+    return scorer;
   }
 
   reportRate(event: any){
-    console.log(this.rate);
-    // TODO hacer petición post para hacer submit del score
+    var request: string;
+    console.log("rate at com " , this.rate)
+    if( this.oldValue == null || typeof this.oldValue == 'undefined'){
+      request = JSON.stringify(
+        {
+          'post_id': this.post.id,
+          /*'user_id': JSON.parse(localStorage.getItem('currentUser')),*/
+          'user_id' : 'default',
+          'scored' : this.rate
+        });
+      this.comService.scorePost(request)
+        .subscribe(
+          () => {}
+        );
+    }else{
+      var scorer = this.getScoreObject();
+      request = JSON.stringify(
+        {
+          'post_id': this.post.id,
+          /*'user_id': JSON.parse(localStorage.getItem('currentUser')),*/
+          'user_id' : 'default',
+          'scored' : this.rate
+        });
+      this.comService.updateScore(scorer[0].id, request)
+        .subscribe();
+    }
   }
 }
