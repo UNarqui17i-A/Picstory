d50dd0f8358ae4bca2f8db366020ffe02f47c9e2
Rodrigo Espinel <riespinelv@unal.edu.co>
2017-04-18 15:28:44 -0500

    removed unused ms

--- backend/ms_post/app/models/post.rb
+++
@@ -1,4 +0,0 @@
-class Post < ApplicationRecord
-    has_many :comments, dependent: :destroy
-    has_many :scores, dependent: :destroy
-end
--- backend/ms_post/app/serializers/score_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ScoreSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :scored, :created_at, :updated_at
-end
