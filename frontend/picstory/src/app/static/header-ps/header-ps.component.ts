6fd670b086758df5b7a85de7f5f0747a552acae1
jufarangoma <juanfe36@hotmail.com>
2017-04-17 20:31:32 -0500

    Update logout button

--- frontend/picstory/src/app/static/header-ps/header-ps.component.ts
+++ frontend/picstory/src/app/static/header-ps/header-ps.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../login/login-app/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-ps',
   templateUrl: './header-ps.component.html',
-  styleUrls: ['./header-ps.component.css']
+  styleUrls: ['./header-ps.component.css'],
+  providers: [AuthenticationService]
 })
 export class HeaderPsComponent implements OnInit {
 
-  constructor() { }
+  public isAuth: boolean;
+
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.isAuth = true;
+  }
 
   public isCollapsed: boolean = false;
 
   ngOnInit() {
+    if (this.authenticationService.token != null) {
+      this.isAuth = false;
+    }
   }
 
+  public redirect(){
+    this.router.navigate(['']);
+  }
+
+  public eraseToken() {
+    this.authenticationService.logout();
+    console.log(this.authenticationService.token);
+  }
 }
