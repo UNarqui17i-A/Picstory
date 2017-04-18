7f37e257253178001446077fe8e455cb22ce9ab2
jufarangoma <juanfe36@hotmail.com>
2017-04-18 05:23:18 -0500

    logout

--- frontend/picstory/src/app/app.component.ts
+++ frontend/picstory/src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import { AuthenticationService } from '../../login/login-app/auth.service'; 
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ ]
+  providers: [ AuthenticationService]
 })
 export class AppComponent {
-  user = JSON.parse(localStorage.getItem('currentUser'));
-  constructor( ){}
-
-  logOut() {
-    localStorage.removeItem(this.user);
-  }
+   public isAuth: boolean; 
+ 
+ 
+  constructor(private authenticationService: AuthenticationService, private router: Router) { 
+    this.isAuth = true; 
+  } 
+  ngOnInit() { 
+    if (this.authenticationService.token != null) { 
+      this.isAuth = false; 
+  } 
+  public eraseToken() { 
+    this.authenticationService.logout(); 
+    console.log(this.authenticationService.token); 
+  } 
 }
