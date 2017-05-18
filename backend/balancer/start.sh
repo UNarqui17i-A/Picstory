279433f9f7a48cdbe6f624fcfbfbf3dc2d7b4c2a
Rodrigo Espinel <riespinelv@unal.edu.co>
2017-05-17 19:30:55 -0500

    added stack names and change port

---
+++ backend/balancer/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+./../rancher-compose --project-name picstory-lb\
+    --url http://192.169.69.2:8080/v1/projects/1a5 \
+    --access-key 4E20D2C6C92278BDF77B \
+    --secret-key qSBR5obJqsg4YVJfnLAhNJarzsySXuc6j1G1Ak5F \
+    -f docker-compose.yml \
+    --verbose up \
+    -d --force-upgrade \
+    --confirm-upgrade
--- frontend/start2.sh
+++ frontend/start2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-rancher-compose --project-name frontend \
+./rancher-compose --project-name frontend \
     --url http://192.169.69.2:8080/v1/projects/1a5 \
     --access-key 4E20D2C6C92278BDF77B \
     --secret-key qSBR5obJqsg4YVJfnLAhNJarzsySXuc6j1G1Ak5F \
