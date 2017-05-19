065536ef243115950b4e8a937b142af0de74022a
Edward Carrillo <eccarrilloe@gmail.com>
2017-05-18 22:14:23 -0500

    feat(balancers): balancer folder created

--- backend/user_ms/start2.sh
+++ backend/user_ms/start2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 ../rancher-compose --project-name user_ms \
-    --url http://192.169.69.2:8080/v1/projects/1a5 \
-    --access-key 4E20D2C6C92278BDF77B \
-    --secret-key qSBR5obJqsg4YVJfnLAhNJarzsySXuc6j1G1Ak5F \
+    --url http://192.169.69.5:8080/v2-beta/projects/1a225 \
+    --access-key 9A8E62940F45454FB3FD \
+    --secret-key 1AvureAbUYC5sXbNvXBR6ccqMGi25txmsEVmvytx \
     -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
