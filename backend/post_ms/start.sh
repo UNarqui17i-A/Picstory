22e6551f8f4e474f302cd38ae25c48c3031f1067
jonathan <jomartinezch@gmail.com>
2017-05-19 03:31:32 -0500

    post master-slave

--- backend/post_ms/start.sh
+++ backend/post_ms/start.sh
@@ -1,7 +1,9 @@
 docker-compose build
 
-docker-compose up -d db
+#docker-compose up -d db
+#docker-compose run --rm app-1 rake db:drop db:create db:migrate db:seed
 
-docker-compose run --rm app rake db:drop db:create db:migrate db:seed
+#docker-compose up -d db-slave
+docker-compose run --rm app-1 rake RAILS_ENV=development db:drop db:create db:migrate db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 docker-compose up
--- backend/post_ms/start2.sh
+++ backend/post_ms/start2.sh
@@ -1,8 +1,8 @@
-./../rancher-compose --project-name post_ms\
-    --url http://192.169.69.2:8080/v1/projects/1a5 \
-    --access-key 4E20D2C6C92278BDF77B \
-    --secret-key qSBR5obJqsg4YVJfnLAhNJarzsySXuc6j1G1Ak5F \
-    -f docker-compose.yml \
+rancher-compose --project-name post_ms\
+    --url http://192.169.69.5:8080/v1/ \
+    --access-key 9A8E62940F45454FB3FD \
+    --secret-key 1AvureAbUYC5sXbNvXBR6ccqMGi25txmsEVmvytx \
+    -f "docker-compose.yml" \
     --verbose up \
     -d --force-upgrade \
     --confirm-upgrade
