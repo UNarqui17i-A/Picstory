a47fc4acb37a145f265b7b715ee594b60b3e898e
Unknown <juanfe36@hotmail.com>
2017-05-19 12:20:17 -0500

    Post Master-Slave

--- backend/post_ms/start3.sh
+++ backend/post_ms/start3.sh
@@ -2,7 +2,7 @@
     --url http://192.168.99.100:8080/v1/projects/1a5 \
     --access-key 2553A14B5035D7A58EF9 \
     --secret-key HYNT9bN2y93xC8MMMGc47YGCEpXqFQVgFSMSqd6n \
-    -f docker-compose2.yml \
+    -f "docker-compose.yml" \
     --verbose up \
     -d --force-upgrade \
     --confirm-upgrade
