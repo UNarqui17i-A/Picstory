8c446ac9f1d80af81ce5c88cf1108e14531ce80b
Edward Carrillo <eccarrilloe@gmail.com>
2017-05-19 13:21:49 -0500

    fix(picstory): working...

--- backend/user_ms/start2.sh
+++ backend/user_ms/start2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 ../rancher-compose --project-name user_ms \
-    --url http://192.169.69.5:8080/v2-beta/projects/1a225 \
-    --access-key 9A8E62940F45454FB3FD \
-    --secret-key 1AvureAbUYC5sXbNvXBR6ccqMGi25txmsEVmvytx \
+    --url http://172.31.40.255:8080/v1/projects/1a5 \
+    --access-key 043C1ED8882C46EA4B54 \
+    --secret-key 2mcR7mBsxyjRsNbsePHi58uTqPhR9XHWw1kvRohE \
     -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
--- frontend/start2.sh
+++ frontend/start2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 ./rancher-compose --project-name frontend \
-    --url http://192.169.69.5:8080/v2-beta/projects/1a225 \
-    --access-key 9A8E62940F45454FB3FD \
-    --secret-key 1AvureAbUYC5sXbNvXBR6ccqMGi25txmsEVmvytx \
+    --url http://172.31.40.255:8080/v2-beta/projects/1a5 \
+    --access-key C92F20F6F47340A0FA90 \
+    --secret-key uznM8DyUZCacnnfeWMi5N13aytq1m5r4iHHjWC9D \
     -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
